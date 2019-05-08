6daa4bee67ee4092736b0fa766e215b1c442975e
Tavish Pegram <tavish@onepeloton.com>
2019-05-08 13:59:08 -0400

    building out charge

--- subscriptions/subscription.py
+++ subscriptions/subscription.py
@@ -1,14 +1,92 @@
 import attr
 import arrow
+import uuid
+
+
+@attr.s(frozen=True)
+class Cents:
+    value: int = attr.ib()
+
+
+@attr.s(frozen=True)
+class Currency:
+    value: str = attr.ib()
+
+
+@attr.s(frozen=True)
+class BillingPeriod:
+    start: arrow.Arrow = attr.ib()
+    end: arrow.Arrow = attr.ib()
+
+
+@attr.s(frozen=True)
+class Money:
+    cents: Cents = attr.ib()
+    currency: Currency = attr.ib()
+
+
+@attr.s(frozen=True)
+class Payment:
+    amount: Money = attr.ib()
+
+    def capture(self):
+        pass
+
+
+@attr.s(frozen=True)
+class Interval:
+    pass
+
+
+@attr.s(frozen=True)
+class Monthly(Interval):
+    pass
+
+
+@attr.s(frozen=True)
+class DefaultPaymentMethod:
+    """
+    Would need to be able to generate new stripe/adyen payments
+    """
+    def create(self, amount: Money):
+        return Payment(
+            amount=amount,
+        )
+
+
+@attr.s(frozen=True)
+class Customer:
+    id: uuid.UUID = attr.ib()
+    default_payment_method: DefaultPaymentMethod = attr.ib()
+
+
+@attr.s(frozen=True)
+class Plan:
+    amount: Money = attr.ib()
+    interval: Interval = attr.ib()
+
+    def create_payment(self, customer: Customer):
+        return customer.default_payment_method.create(
+            amount=self.amount,
+        )
 
 
 @attr.s(frozen=True)
 class Subscription:
-    current_billing_period = attr.ib()
+    current_billing_period: BillingPeriod = attr.ib()
+    customer: Customer = attr.ib()
+    plan: Plan = attr.ib()
 
 
     def bill(self):
         now = arrow.utcnow()
+
+        payment = self.plan.create_payment(
+            self.customer,
+        )
+
+        payment.capture()
+
         return attr.evolve(
             self,
             current_billing_period=BillingPeriod(
@@ -16,25 +94,3 @@ class Subscription:
                 end=now.shift(months=+1),
             )
         )
-
-
-@attr.s(frozen=True)
-class BillingPeriod:
-    start = attr.ib()
-    end = attr.ib()
-
-@attr.s(frozen=True)
-class Payment:
-    pass
-
-@attr.s(frozen=True)
-class Money:
-    pass
-
-@attr.s(frozen=True)
-class Cents:
-    pass
-
-@attr.s(frozen=True)
-class Currency:
-    pass
--- subscriptions/test_bill_subscription.py
+++ subscriptions/test_bill_subscription.py
@@ -6,10 +6,14 @@ from unittest import (
 )
 from .subscription import (
     BillingPeriod,
+    DefaultPaymentMethod,
     Subscription,
     Money,
     Cents,
     Currency,
+    Plan,
+    Monthly,
+    Customer,
 )
 
 
@@ -20,8 +24,21 @@ class TestBillSubscription(TestCase):
             self,
             m_payment,
     ):
+        m_payment = mock.MagicMock()
+
         subscription = Subscription(
             current_billing_period=None,
+            customer=Customer(
+                id="123",
+                default_payment_method=DefaultPaymentMethod()
+            ),
+            plan=Plan(
+                amount=Money(
+                    cents=Cents(1945),
+                    currency=Currency("USD")
+                ),
+                interval=Monthly()
+            )
         )
 
         now = arrow.utcnow()
@@ -31,17 +48,22 @@ class TestBillSubscription(TestCase):
         ):
             subscription = subscription.bill()
 
-        m_payment.capture.assert_called_once_with(
-            Money(
-                amount=Cents(1945),
-                currency=Currency("USD")
-            )
-        )
-
+        m_payment.capture.assert_called_once()
 
     def test_bill_a_new_sub_updates_billing_period(self):
         subscription = Subscription(
             current_billing_period=None,
+            customer=Customer(
+                id="123",
+                default_payment_method=DefaultPaymentMethod()
+            ),
+            plan=Plan(
+                amount=Money(
+                    cents=Cents(1945),
+                    currency=Currency("USD")
+                ),
+                interval=Monthly()
+            )
         )
 
         now = arrow.utcnow()
@@ -57,6 +79,17 @@ class TestBillSubscription(TestCase):
                 current_billing_period=BillingPeriod(
                     start=now,
                     end=now.shift(months=+1),
+                ),
+                customer=Customer(
+                    id="123",
+                    default_payment_method=DefaultPaymentMethod()
+                ),
+                plan=Plan(
+                    amount=Money(
+                        cents=Cents(1945),
+                        currency=Currency("USD")
+                    ),
+                    interval=Monthly()
                 ),
             )
         )
